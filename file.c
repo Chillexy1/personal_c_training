466583e4065edefc4973ceb7d5a2da1445c892ce
chillexy <chillexystev96@gmail.com>
2026-05-17 16:35:23 +0100

    learning to use c for text edition

---
+++ file.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+ *creating a file using c
+ * */
+
+int main(void) {
+  FILE *pFile = fopen("text.txt", "w");
+  char text[] = "LEARNING SOFTWARE ENGINEERING AT WINMINGLE IS THE "
+                "BEST\n\nWELDONE CHILLEXY!!!\n ";
+
+  if (pFile == NULL) {
+    printf("error fetching file\n");
+    return 1;
+  }
+
+  // fprintf is best practice to print file before outputing with printf or any
+  // other standard output function
+
+  fprintf(pFile, "%s", text);
+  printf("file was successfuly written\n");
+  fclose(pFile);
+
+  return 0;
+}
